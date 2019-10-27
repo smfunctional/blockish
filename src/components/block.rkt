31d70f848049c7ade23b323b3088a5585572f616
smdeveloper <smazumder@gmail.com>
2019-10-27 18:53:06 -0400

    added mine methods for block.

--- src/components/block.rkt
+++ src/components/block.rkt
@@ -4,23 +4,42 @@
 (require (only-in sha bytes->hex-string))
 (require racket/serialize)
 
-(struct block (hash previous-hash current-data timestamp nonce) #:prefab)
+; A Block should contain the current hash, the previous hash, data, and timestamp when it was generated:
+; 
+(struct block (hash previous-hash transaction timestamp nonce) #:prefab)
 
 ; calculate block hash
-(define (calc-block-hash previous-hash current-data timestamp nounce)
+(define (calc-block-hash previous-hash transaction timestamp nonce)
   (bytes->hex-string (sha256 (bytes-append
                               (string->bytes/utf-8 previous-hash)
                               (string->bytes/utf-8 (number->string timestamp))
-                              (string->bytes/utf-8 (~a (serialize current-data)))
-                              (string->bytes/utf-8 (number->string nounce))))))
+                              (string->bytes/utf-8 (~a (serialize transaction)))
+                              (string->bytes/utf-8 (number->string nonce))))))
 
 ; Check to see if the given block is a valid block and is a memeber of the chain
 (define (is-valid-block? blck)
   ; check to see of the hash is the same as this block
   (equal? (block-hash blck)
           (calc-block-hash (block-previous-hash blck)
-                           (block-current-data blck)
+                           (block-transaction blck)
                            (block-timestamp blck)
-                           (block-nounce blck))))
+                           (block-nonce blck))))
 
-(provide (struct-out block) valid-block?)
+; A block is mined if the hash matches the target, given the difficulty.
+(define difficulty 2)
+(define target (bytes->hex-string (make-bytes difficulty 32)))
+
+(define (mined-block? bl-hash)
+  (equal? (subbytes (hex-string->bytes bl-hash) 1 difficulty)
+          (subbytes (hex-string->bytes target) 1 difficulty)))
+
+(define (build-and-mine target previous-hash timestamp transaction nonce)
+  (let ([hash (calc-block-hash previous-hash timestamp transaction nonce)])
+    (if (mined-block? hash)
+        (block hash previous-hash transaction timestamp nonce)
+        (build-and-mine target previous-hash transaction timestamp (+ nonce 1)))))
+
+(define (mine-block transaction previous-hash)
+  (build-and-mine target previous-hash transaction (current-milliseconds) 1))
+
+(provide (struct-out block) is-valid-block? mine-block)
